d6720eb65a876d1a8e2e002cc03996135f894bb6
Sandesh <sandesh.pandey00112@gmail.com>
2025-02-15 15:32:30 +0545

    Tic-Tac-Toe completed

--- indexx.js
+++ indexx.js
@@ -1,35 +1,73 @@
 let boxes = document.querySelectorAll(".box");
 let resetbtn = document.querySelector("#reset-btn");
+let newbtn = document.querySelector("#new-btn");
+let msgcont = document.querySelector(".msg-cont");
+let msg = document.querySelector("#msg");
+let turn = true; // Fixed typo from 'trun' to 'turn'
 
-let trun= true; 
+const winpatterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+    [0, 4, 8], [2, 4, 6] 
+];
 
-const winpatteerns =[
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,6],
-    [2,4,6]    
-]
+//Clears and enables boxes
+const enableBoxes = () => {
+    boxes.forEach(box => {
+        box.innerText = ""; // Clears X and O
+        box.disabled = false; // Enable clicks
+    });
+};
+
+const disableBoxes = () => {
+    boxes.forEach(box => {
+        box.disabled = true;
+    });
+};
 
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
-        console.log("box was being clicked by you");
-        if (trun) {
+        console.log("Box was clicked!");
+        if (turn) {
             box.innerText = "X";
-            trun= false;
-        }else{
+            turn = false;
+        } else {
             box.innerText = "O";
-            trun = true;
+            turn = true;
         }
         box.disabled = true;
-
+        checkWinner(); // Calling winner function
     });
 });
 
-const checkwinner = () => {
-    for pos1val = boxes{pattern}
-    
-}
+const displayWinner = (winner) => {
+    msg.innerText = `Congratulations! Winner is ${winner}`; // Fixed typo
+    msgcont.classList.remove("hide");
+    disableBoxes();
+};
+
+const checkWinner = () => {
+    winpatterns.forEach(pattern => {
+        let val1 = boxes[pattern[0]].innerText;
+        let val2 = boxes[pattern[1]].innerText;
+        let val3 = boxes[pattern[2]].innerText;
+
+        if (val1 !== "" && val1 === val2 && val2 === val3) {
+            console.log("Winner detected:", val1);
+            displayWinner(val1);
+        }
+    });
+};
+
+const btnReset = () => {
+    turn = true;
+    msgcont.classList.add("hide"); // Hide message
+    boxes.forEach(box => {
+        box.innerText = ""; // Clear text
+        box.disabled = false; // Enable clicks
+    });
+};
+
+//event listeners
+newbtn.addEventListener("click", btnReset);
+resetbtn.addEventListener("click", btnReset);
